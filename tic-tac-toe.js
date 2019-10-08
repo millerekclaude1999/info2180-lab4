b7373953fa6fa310b25389ed699a0d6d8dd047b3
Claude Miller <millerclaude1999@gmail.com>
2019-10-08 10:38:22 -0500

    Added a js file

---
+++ tic-tac-toe.js
@@ -0,0 +1,15 @@
+var game;
+var status;
+var board;
+var controls;
+var btn;
+window.onload = function(){
+    game = document.querySelectorAll("#game");
+    status = document.querySelectorAll("#status");
+    board = document.querySelectorAll("#board");
+    controls = document.querySelectorAll(".controls");
+    if(div.classList.contains(null)){
+        div.setAttribute(".square");
+    }
+    
+}
