3b12b82c893f07ec8abf41dc0769e3213af4c8b0
Claude Miller <millerclaude1999@gmail.com>
2019-10-08 12:09:47 -0500

    updated js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,13 +3,18 @@ var status;
 var board;
 var controls;
 var btn;
+var divs;
 window.onload = function(){
     game = document.querySelectorAll("#game");
     status = document.querySelectorAll("#status");
     board = document.querySelectorAll("#board");
     controls = document.querySelectorAll(".controls");
-    if(div.classList.contains(null)){
-        div.setAttribute(".square");
-    }
-    
-}
+    divs = document.getElementsByTagName("div");
+    for(let i=0; i<divs.length;i++){
+        if(divs[i].id==""){
+            if(divs[i].className==""){
+                this.divs[i].classList.add("square");
+            }
+        }
+    }  
+} 
