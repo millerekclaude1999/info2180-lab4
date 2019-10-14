ccca9d0826d9409f048d23e38d768d7bffce68df
Claude Miller <millerclaude1999@gmail.com>
2019-10-14 00:25:56 -0500

    Adjusted js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,17 +4,281 @@ var board;
 var controls;
 var btn;
 var divs;
-window.onload = function(){
+var divs1;
+var factions;
+var gameOver;
+var turns=0;
+window.onload =function(){
+
+
+
+
+
     game = document.querySelectorAll("#game");
-    status = document.querySelectorAll("#status");
+    status = document.getElementById("status");
     board = document.querySelectorAll("#board");
     controls = document.querySelectorAll(".controls");
     divs = document.getElementsByTagName("div");
-    for(let i=0; i<divs.length;i++){
-        if(divs[i].id==""){
-            if(divs[i].className==""){
-                this.divs[i].classList.add("square");
-            }
+    divs1 = document.getElementsByClassName("square");
+    factions = new Array("X","O");
+    for(let i=0;i<divs.length;i++){
+        if((divs[i].id=="") && (divs[i].className=="")){
+            divs[i].classList.add("square");
+            divs[i].onmouseenter=function(){
+                styleOn(divs[i]);
+            };
+            divs[i].onmouseleave=function(){
+                styleOff(divs[i]);
+            };
         }
-    }  
-} 
+        if(divs[i].className=="square"){
+            divs[i].onclick=function(){
+                emptyOut(divs[i]);
+                gameCheck();
+            };
+        }
+    }
+
+
+
+
+btn=document.querySelector("button");
+
+
+
+
+
+btn.onclick =function(){
+
+
+Vanish();
+
+
+
+Vanish1();
+
+};
+
+
+function gameCheck(){
+    if(gameOver){
+        for( var k=0; k<divs1.length;k++){
+            divs1[k].onclick=" ";
+        }
+    }
+}
+
+
+
+
+
+function 
+Vanish(){
+
+gameOver=false;
+
+for(var i=0;i<divs1.length;i++){
+
+
+
+divs1[i].textContent =
+" ";
+divs1[i].onclick=function(){
+    emptyOut(divs1[i])
+};
+if(divs1[i].classList.contains("X")){
+    divs1[i].classList.remove("X");
+}
+else{
+    divs1[i].classList.remove("O");
+}
+
+}
+
+
+
+turns=0;
+
+
+
+}
+
+
+
+
+
+function 
+Vanish1(){
+
+
+
+for(varj=0;j<this.divs1.length;j++){
+
+
+
+if(divs[j].id=="status"){
+
+
+
+divs[j].textContent="Move your mouse over a square and click to play an X or an O.";
+
+
+
+}
+
+}
+
+}
+
+
+
+
+
+
+function 
+change(element){
+
+
+if(turns==0){
+
+element.innerHTML="X";
+element.classList.add("X");
+
+
+turns=1;
+
+
+
+
+}
+
+
+
+else{
+
+element.innerHTML="O";
+element.classList.add("O");
+
+
+turns=0;
+
+
+
+}
+
+
+
+}
+
+
+
+function 
+emptyOut(element){
+
+var 
+check=element.innerHTML;
+
+
+
+if(check=="X"||check=="O"){
+
+
+
+}
+
+
+
+else{
+
+
+
+change(element);
+
+winner();
+
+}
+
+
+
+}
+
+function winner(){
+    status = 
+    document.getElementById("status");
+for(var i = 0;i<divs.length;i++){
+
+if(divs[i].id=="status"){
+
+if((divs1[0].innerHTML=="X" &&
+divs1[1].innerHTML=="X" &&
+divs1[2].innerHTML=="X") || (divs1[3].innerHTML=="X"
+ && divs1[4].innerHTML=="X" &&
+divs1[5].innerHTML=="X") || (divs1[6].innerHTML=="X"
+ && divs1[7].innerHTML=="X" &&
+divs1[8].innerHTML=="X")||(divs1[0].innerHTML=="X"
+ && divs1[3].innerHTML=="X" &&
+divs1[6].innerHTML=="X")||(divs1[1].innerHTML=="X"
+ && divs1[4].innerHTML=="X" &&
+divs1[7].innerHTML=="X")||(divs1[2].innerHTML=="X"
+ && divs1[5].innerHTML=="X" &&
+divs1[8].innerHTML=="X")||(divs1[0].innerHTML=="X"
+ && divs1[4].innerHTML=="X" &&
+divs1[8].innerHTML=="X")||(divs1[0].innerHTML=="2"
+ && divs1[4].innerHTML=="X" &&
+divs1[6].innerHTML=="X")){
+divs[i].textContent="Congratulations! X is the winner.";
+gameOver=true;
+}
+
+else 
+if((divs1[0].innerHTML=="O" &&
+divs1[1].innerHTML=="O" &&
+divs1[2].innerHTML=="O") || (divs1[3].innerHTML=="O"
+ && divs1[4].innerHTML=="O" &&
+divs1[5].innerHTML=="O") || (divs1[6].innerHTML=="O"
+ && divs1[7].innerHTML=="O" &&
+divs1[8].innerHTML=="O")||(divs1[0].innerHTML=="O"
+ && divs1[3].innerHTML=="O" &&
+divs1[6].innerHTML=="O")||(divs1[1].innerHTML=="O"
+ && divs1[4].innerHTML=="O" &&
+divs1[7].innerHTML=="O")||(divs1[2].innerHTML=="O"
+ && divs1[5].innerHTML=="O" &&
+divs1[8].innerHTML=="O")||(divs1[0].innerHTML=="O"
+ && divs1[4].innerHTML=="O" &&
+divs1[8].innerHTML=="O")||(divs1[2].innerHTML=="O"
+ && divs1[4].innerHTML=="O" &&
+divs1[6].innerHTML=="O")){
+divs[i].textContent="Congratulations! O is the winner.";
+status.classList.add("you-win");
+gameOver=true;
+
+}
+
+}
+
+}
+
+}
+
+function 
+styleOn(element){
+
+
+
+element.classList.add("hover");
+
+
+}
+
+function 
+styleOff(element){
+
+
+
+element.classList.remove("hover");
+
+
+
+}
+
+}
